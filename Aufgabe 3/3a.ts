a311c52218a084dbc28b40a93096a9b9d81f93d9
Alexander <Alexander@Alexander-PC>
2017-04-19 21:34:42 +0200

    Aufgabe verbessert

--- Aufgabe 3/3a.ts
+++ Aufgabe 3/3a.ts
@@ -1,18 +1,27 @@
 //Aufgabe: Aufgabe 3a "Sissas Summe"
 //Name: Kristina Novikov
 //Matrikel: 254136
-//Datum: 09.04.2017
+//Datum: 19.04.2017
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 
-//Diesen Code habe ich nicht ganz alleine geschrieben, ich habe viel recherchiert und ausprobiert
-//Im Grunde verstehe ich den Code, aber ich brauche immer etwas Hilfe und Unterstütung um voranzukommen
-//Zudem habe ich es leider nicht hinbekommen, dass die Markierung bei erneutem Klick wieder verschwindet
-//und einen Tooltip zu erstellen
+
+// Verbesserung der Aufgabe
+
 
 
 document.addEventListener("DOMContentLoaded", function() {
 
+
+    // Tooltip erstellen
+    let toolTip: HTMLElement = document.createElement("span");
+    document.body.appendChild(toolTip);
+    toolTip.setAttribute("id", "tooltip");
+
+
+
     let white: boolean = true;
     let reisKoerner: number = 1;
+    let summe: number = 0;
 
     for (let zeile: number = 0; zeile < 8; zeile++) {
 
@@ -42,51 +51,61 @@ document.addEventListener("DOMContentLoaded", function() {
         else {
             white = true;
         }
+
+        reisKoerner *= 2;
     }
-    
-    
-    
-    // Aufgabe 3a - Felder markieren //
 
-   
-    let div: NodeListOf<HTMLDivElement> = document.getElementsByTagName("div");
 
-    for (let markiert: number = 0; markiert < 9; markiert++) {
-        div[markiert].addEventListener("click", markierungSetzen);
 
-        let markierung: boolean = true;
+// Aufgabe 3a - Felder markieren //
 
-        function markierungSetzen() {
-            if (markierung == true) {
-                div[markiert].style.backgroundColor = "lightblue";
-                markierung = false;
-            }
+    // Nur die Felder der ersten Reihe sollen markiert werden
+    let felderErsteReihe = document.getElementsByTagName("div");
 
-            else {
-                div[markiert].style.backgroundColor = "lightblue";
-                markierung = false;
-            }
-            if (markierung == true) {
-                markierung = !markierung;
-            }
+    for (let f: number = 0; f < 8; f++) {
+        felderErsteReihe[f].addEventListener("click", function() {
+            markierungSetzen(felderErsteReihe[f]);
+        });
+    }
 
-            else {
-                markierung = true;
-            }
+    // Angeklickte Felder farbig markieren
+    function markierungSetzen(geklicktesFeldMarkieren: HTMLElement) {
+        let markierteFelder = document.getElementsByClassName("markiert");
+
+        geklicktesFeldMarkieren.classList.toggle("markiert");
+
+
+
+
+// Aufgabe 3a - Tooltip über markierten Feldern //
+        
+        // Tooltip anzeigen, wenn ein oder mehrere Felder markiert sind, ansonsten nicht anzeigen
+        if (markierteFelder.length >= 1) {
+            document.getElementById("tooltip").style.display = "inline-block";
         }
+        else {
+            document.getElementById("tooltip").style.display = "none";
+        }
+
+
+
+        // Inhalt des Tooltips ist die Summe der Reiskörner in Dezimal und in Hexadezimalzahlen
+        
+
+        for (let t: number = 0; t < markierteFelder.length; t++) {
+            summe += Number(markierteFelder[t].textContent);
+            let dezimal: string = summe.toString();
+            let hexadezimal: string = summe.toString(16);
+            toolTip.textContent = "Summe der Reiskoerner " + "Dezimal: " + dezimal + "\r\n" + "Hexadezimal: " + hexadezimal;
+        }
+
+
     }
+
+    // Tooltip soll sich mit Maucusor mitbewegen
+    document.onmousemove = function(_event: MouseEvent): void {
+        toolTip.style.top = (_event.clientY + 8) + "px";
+        toolTip.style.left = (_event.clientX + 10) + "px";
+    };
+
 });
-    
-
-
-
-
-    // Tooltip mit Mauscursor //
-
-//    document.addEventListener("mouseover", function(Event) {
-//        document.getElementById("SummeReiskoerner").style.left = (Event.clientX + 10) + "px";
-//        document.getElementById("SummeReiskoerner").style.top = (Event.clientY + 10) + "px";
-//});
-
-
-
