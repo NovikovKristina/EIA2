473a4a233561cf7b275318cd5780a82737567a23
Alexander <Alexander@Alexander-PC>
2017-05-06 00:56:25 +0200

    6b

---
+++ Aufgabe 6/6b.js
@@ -0,0 +1,56 @@
+var StudiVZ;
+(function (StudiVZ) {
+    var students = [];
+    var stop = false;
+    while (!stop) {
+        var action = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+        switch (action) {
+            case "n":
+            case "N":
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 f�r weiblich oder 1 f�r m�nnlich) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+    function saveData(_input) {
+        let dataArray = _input.split(",");
+        let sD = {
+            matrikelnumber: parseInt(dataArray[0]),
+            lastname: dataArray[1],
+            firstname: dataArray[2],
+            age: parseInt(dataArray[3]),
+            gender: parseInt(dataArray[4]) == 1,
+            comment: dataArray[5]
+        };
+        students.push(sD);
+        let gender;
+        if (parseInt(dataArray[4]) == 0) {
+            gender = "weiblich";
+        }
+        else {
+            gender = "m�nnlich";
+        }
+        // Ausgabe der eingegebenen Daten
+        return "Deine eingegebenen Daten:\n" + "\nMatrikelnummer: " + sD.matrikelnumber + "\nName: " + sD.lastname + "," + sD.firstname + "\nAlter: " + sD.age + "\nGeschlecht: " + gender + "\nKommentar: " + sD.comment;
+    }
+    function queryData(_matrikel) {
+        for (let i = 0; i < students.length; i++) {
+            if (students[i].matrikelnumber == _matrikel) {
+                let gender = students[i].gender ? "m�nnlich" : "weiblich";
+                return "Zugeh�rige Daten zur Matrikelnummer: " + students[i].matrikelnumber + "\n\nName: " + students[i].lastname + "," + students[i].firstname + "\nAlter: " + students[i].age + "\nGeschlecht: " + gender + "\nKommentar: " + students[i].comment;
+            }
+            else {
+                return "Die eingegebene Matrikelnummer existiert nicht";
+            }
+        }
+    }
+})(StudiVZ || (StudiVZ = {}));
+//# sourceMappingURL=6b.js.map
---
+++ Aufgabe 6/6b.ts
@@ -0,0 +1,76 @@
+namespace StudiVZ {
+    
+    interface StudentData {
+        matrikelnumber: number;
+        lastname: string;
+        firstname: string;
+        age: number;
+        gender: boolean;
+        comment: string;
+    }
+
+    var students: StudentData[] = [];
+    var stop: boolean = false;
+
+    while (!stop) {
+        var action: string = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+
+        switch (action) {
+            case "n":
+            case "N":
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 für weiblich oder 1 für männlich) und Kommentar");
+                alert(saveData(input));
+                break;
+                
+            case "a":
+            case "A":
+                var matrikel: number = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+                
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+
+    function saveData(_input: string): string {
+        let dataArray: string[] = _input.split(",");
+        let sD: StudentData = {
+            matrikelnumber: parseInt(dataArray[0]),
+            lastname: dataArray[1],
+            firstname: dataArray[2],
+            age: parseInt(dataArray[3]),
+            gender: parseInt(dataArray[4]) == 1,
+            comment: dataArray[5]
+        };
+
+        students.push(sD);
+
+        let gender: string;
+        if (parseInt(dataArray[4]) == 0) {
+            gender = "weiblich";
+        }
+        else {
+            gender = "männlich";
+        }
+
+        // Ausgabe der eingegebenen Daten
+        return "Deine eingegebenen Daten:\n" + "\nMatrikelnummer: " + sD.matrikelnumber + "\nName: " + sD.lastname + "," + sD.firstname + "\nAlter: " + sD.age + "\nGeschlecht: " + gender + "\nKommentar: " + sD.comment;
+    }
+
+    function queryData(_matrikel: number): string {
+
+        for (let i: number = 0; i < students.length; i++) {
+
+            if (students[i].matrikelnumber == _matrikel) {
+                let gender: string = students[i].gender ? "männlich" : "weiblich";
+                return "Zugehörige Daten zur Matrikelnummer: " + students[i].matrikelnumber + "\n\nName: " + students[i].lastname + "," + students[i].firstname + "\nAlter: " + students[i].age + "\nGeschlecht: " + gender + "\nKommentar: " + students[i].comment;
+            }
+
+            else {
+                return "Die eingegebene Matrikelnummer existiert nicht";
+            }
+        }
+    }
+}
