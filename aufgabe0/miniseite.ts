0468871f6c112c8bc6ca4d37f2843bcff209019a
Admin <Admin@141.28.130.151>
2017-10-11 12:20:30 +0200

    aufgabe0

--- aufgabe0/miniseite.ts
+++ aufgabe0/miniseite.ts
@@ -1,11 +1,11 @@
 /* Aufgabe: Aufgabe 0 "Kleine interaktive Webseite"
 Name: Kristina Novikov
 Matrikel: 254136
-Datum: 10.10.2017
+Datum: 11.10.2017
 
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert. */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
 
 let greeter: string = prompt ("Hier bitte Ihren Namen eingeben");
 
@@ -16,7 +16,7 @@ if (greeter != null) {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
 
 document.getElementById("style").style.color = "#4D1933";
 document.getElementById("style").style.fontSize = "3em";
@@ -24,4 +24,4 @@ document.getElementById("style").style.fontFamily = "sans-serif";
 document.getElementById("style").style.textAlign = "center";
 document.getElementById("style").style.backgroundColor = "#FFF8E7";
     
-}); 
+});
