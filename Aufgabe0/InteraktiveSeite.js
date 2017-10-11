988c63452239ce4fe827b8e9bb0229a31659de7b
Admin <Admin@141.28.130.151>
2017-10-11 12:02:10 +0200

    Aufgabe0

---
+++ Aufgabe0/InteraktiveSeite.js
@@ -0,0 +1,20 @@
+/* Aufgabe: Aufgabe 0 "Kleine interaktive Webseite"
+Name: Kristina Novikov
+Matrikel: 254136
+Datum: 10.10.2017
+
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert. */
+document.addEventListener('DOMContentLoaded', function () {
+    let greeter = prompt("Hier bitte Ihren Namen eingeben");
+    if (greeter != null) {
+        document.getElementsByTagName("div")[0].innerHTML = "Hallo " + greeter + "! :-)";
+    }
+});
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("style").style.color = "#4D1933";
+    document.getElementById("style").style.fontSize = "3em";
+    document.getElementById("style").style.fontFamily = "sans-serif";
+    document.getElementById("style").style.textAlign = "center";
+    document.getElementById("style").style.backgroundColor = "#FFF8E7";
+});
+//# sourceMappingURL=InteraktiveSeite.js.map
---
+++ Aufgabe0/InteraktiveSeite.ts
@@ -0,0 +1,27 @@
+/* Aufgabe: Aufgabe 0 "Kleine interaktive Webseite"
+Name: Kristina Novikov
+Matrikel: 254136
+Datum: 10.10.2017
+
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert. */
+
+document.addEventListener('DOMContentLoaded', function() {
+
+let greeter: string = prompt ("Hier bitte Ihren Namen eingeben");
+
+if (greeter != null) {
+    document.getElementsByTagName("div")[0].innerHTML = "Hallo " + greeter + "! :-)";
+}   
+      
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+document.getElementById("style").style.color = "#4D1933";
+document.getElementById("style").style.fontSize = "3em";
+document.getElementById("style").style.fontFamily = "sans-serif";
+document.getElementById("style").style.textAlign = "center";
+document.getElementById("style").style.backgroundColor = "#FFF8E7";
+    
+}); 
